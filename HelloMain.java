ae3a97b8e8105ed41586ec92637505ed14969c72
yjin <yjin@s11.etu.info-ufr.univ-montp2.fr>
2013-09-19 10:41:59 +0200

    HelloMain TP1

---
+++ HelloMain.java
@@ -0,0 +1,12 @@
+
+public class HelloMain
+{
+	public static void main(String[] args)
+	{
+		HelloWorld h1 = new HelloWorld("JIN"); 
+		HelloWorldGender h2 =  new HelloWorldGender("JIN",true);
+			
+		h1.print();
+		h2.print();
+	}
+}
