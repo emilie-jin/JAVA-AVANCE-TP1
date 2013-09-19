b6a36c93b6a2d7b58a387fa2a4a106a6824b6489
yjin <yjin@s11.etu.info-ufr.univ-montp2.fr>
2013-09-19 10:38:12 +0200

    HelloWorld TP1

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld
+{
+	public String nom;
+
+	public HelloWorld(String nom)
+	{
+		this.nom=nom;
+	}
+
+	public void print()
+	{
+		System.out.println("Hello "+this.nom+" !!!");
+	}
+}
+
+
